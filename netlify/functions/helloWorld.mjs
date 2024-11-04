5167a79d612b506df5392d6137e80d8e84f431c0
k.b.Yudhees Raam Kumar <yudees.raamkumar@bakuun.com>
2024-11-04 12:51:59 +0530

    bug fixture

--- netlify/functions/helloWorld.mjs
+++ netlify/functions/helloWorld.mjs
@@ -1,9 +1,9 @@
-// import { getDb } from "#config/mongodb.js";
+import { getDb } from "#config/mongodb.js";
 
-// const db=await getDb()
+const db=await getDb()
 export default async (req, context) => {
-  // const data=await db.collection('contents').find().toArray()
-  const data={name:'yudhees'}
+  const data=await db.collection('contents').find().toArray()
+  // const data={name:'yudhees'}
   return new Response(JSON.stringify(data),{ headers: {
     'Content-Type': 'application/json', 
   },})
