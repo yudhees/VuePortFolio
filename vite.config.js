cd3dcf9f25d09c5d65634d116514de4248219cd0
k.b.Yudhees Raam Kumar <125550173+yudhees@users.noreply.github.com>
2024-11-08 20:15:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'front', // Ensure this matches the directory name you set in Vercel
+  },
 })
